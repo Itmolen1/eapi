e0a408b912414e4096384a8a4af61aee26a4619a
unknown <itmolen1@gmail.com>
2019-09-08 14:58:02 +0400

    migration created for product and reviews

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+//use Illuminate\Routing\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/products','ProductController');
+Route::group(['prefix'=>'products'],function () {
+
+    Route::apiResource('/{product}/reviews', 'ReviewController');
+});
+
+
